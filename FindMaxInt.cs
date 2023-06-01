6c0370337f7763e0b3a0af7577ecad076f3a2805
Vedant <vedantnp200@gmail.com>
2023-06-01 15:02:51 +0530

    Find maximum interger from among three numbers

---
+++ FindMaxInt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Maximum_Using_Generics
+{
+    internal class FindMaxInt
+    {
+        // UC1 - Given three integers, find the maximum
+        public static int MaxInteger(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else
+            {
+                return third;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,42 @@ namespace Test_Maximum_Using_Generics
         static void Main(string[] args)
         {
 
+            while (true)
+            {
+
+                Console.WriteLine("Enter an option: ");
+                Console.WriteLine("1. Find the maximum integer.");
+                Console.WriteLine("2. Find the maximum float.");
+                Console.WriteLine("3. Find the maximum string.");
+                Console.Write("=> ");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+
+                    case 1:
+                        // TC 1.1 - Max Number at first position
+                        int maxFirstInt = FindMaxInt.MaxInteger(45, 12, 3);
+                        Console.WriteLine("{0} is the maximum number at first position", maxFirstInt);
+
+                        // TC 1.2 - Max Number at second position
+                        int maxSecondInt = FindMaxInt.MaxInteger(12, 34, 2);
+                        Console.WriteLine("{0} is the maximum number at second position", maxSecondInt);
+
+                        // TC 1.3 -  Max Number at third position
+                        int maxThirdInt = FindMaxInt.MaxInteger(3, 9, 19);
+                        Console.WriteLine("{0} is the maximum number at third position", maxThirdInt);
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter valid option.");
+                        break;
+
+                }
+
+            }
+
         }
     }
 }
