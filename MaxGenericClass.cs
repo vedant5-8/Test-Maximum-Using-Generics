4d78a25262cb8583b1b5febfecbf22cce13e76da
Vedant <vedantnp200@gmail.com>
2023-06-01 16:21:22 +0530

    Create a generic class and find maximum value

---
+++ MaxGenericClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Maximum_Using_Generics
+{
+    internal class MaxGenericClass<T> where T : IComparable<T>
+    {
+        // Create generic class and find the maximum value
+        public static T MaxValue(T first, T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else
+            {
+                return third;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace Test_Maximum_Using_Generics
                 Console.WriteLine("1. Find the maximum integer.");
                 Console.WriteLine("2. Find the maximum float.");
                 Console.WriteLine("3. Find the maximum string.");
-                Console.WriteLine("4. Find the maximum value using Generics");
+                Console.WriteLine("4. Find the maximum value using Generics Method");
+                Console.WriteLine("5. Find the maximum value using Generics Class");
                 Console.WriteLine("0. Exit");
                 Console.Write("=> ");
 
@@ -77,6 +78,20 @@ namespace Test_Maximum_Using_Generics
                         // TC 1.3 -  Max string from among three strings
                         string MaxValue3 = MaxValueWithGenerics.MaxValue("Programming", "Oriented", "Object");
                         Console.WriteLine("{0} is the maximum string at third position", MaxValue3);
+                        break;
+
+                    case 5:
+                        // TC 1.1 - Max int from among three integers
+                        int MaxValue4 = MaxGenericClass<int>.MaxValue(43456, 456876, 367654336);
+                        Console.WriteLine("{0} is the maximum number at first position", MaxValue4);
+
+                        // TC 1.2 - Max float from among three float numbers
+                        float MaxValue5 = MaxGenericClass<float>.MaxValue(234.567f, 5986.0978f, 1254.6758f);
+                        Console.WriteLine("{0} is the maximum float at second position", MaxValue5);
+
+                        // TC 1.3 -  Max string from among three strings
+                        string MaxValue6 = MaxGenericClass<string>.MaxValue("Programming", "Oriented", "Object");
+                        Console.WriteLine("{0} is the maximum string at third position", MaxValue6);
                         break;
 
                     case 0:
